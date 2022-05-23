b3b8f8c41555361569fb9148d9721fdd50999d3a
Carlos Eduardo dos Santos <ca_eduardo85@hotmail.com>
2022-05-23 16:01:49 -0300

    Soma, aposentadoria

---
+++ SomaDosNumeros.js
@@ -0,0 +1,34 @@
+function SomaDosNumeros ()
+{
+let Num1 = parseInt = (prompt ("Digite o numero" ));
+let Num2 = parseInt = (prompt ("Digite o numero" ));
+let Num3 = parseInt = (prompt ("Digite o numero" ));
+let Resultado = parseFloat 
+
+if (Num1 < Num2 )  (Num1 < Num3) 
+    document.write (NumMaior1  <= Num2)
+    document.write (NumMaior2 <= Num3)
+        alert == (Num1 + Num2 && Num1 + Num3 )
+        NumMaior1 + NumMaior2 == Resultado
+ {
+    (Num2 < Num1 )  (Num2 < Num3) 
+        document.write (NumMaior2 <= Num1)
+        document.write (NumMaior2 <= Num3)
+        alert == (Num2 + Num1 && Num2 + Num3)
+        NumMaior1 + NumMaior2 == Resultado
+    }
+         {
+             (Num3 < Num1 )  (Num3 < Num2) 
+                document.write (NumMaior2 <= Num1)
+                document.write (NumMaior2 <= Num2)
+                alert == (Num3 + Num1 && Num3 + Num2)
+                NumMaior1 + NumMaior2 == Resultado
+        }
+
+        NumMaior1 + NumMaior2 == Resultado
+
+document.write ("Maiores Números são: ", NumMaior1, " e ", NumMaior2)
+
+document.write ("Soma: ", Soma)
+
+}   
